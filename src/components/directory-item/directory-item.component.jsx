3c0b339859572c36de7c7b9f3de0f0c1cede76ab
scrob-stefan <stefan.scrob@lola.tech>
2023-01-31 21:13:31 +0200

    latest commit

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,13 +1,13 @@
-import './category-item.styles.scss';
+import './directory-item.styles.scss';
 
-const CategoryItem = ({ id, category }) => {
+const DirectoryItem = ({ id, category }) => {
     const { imageUrl, title } = category;
     return (
-        <div key={id} className='category-container'>
+        <div key={id} className='directory-item-container'>
             <div className='background-image' style={{
                 backgroundImage: `url(${imageUrl})`
             }} />
-            <div className='category-body-container'>
+            <div className='body'>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
@@ -15,4 +15,4 @@ const CategoryItem = ({ id, category }) => {
     );
 }
 
-export default CategoryItem
+export default DirectoryItem
--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,19 +1,17 @@
-import { useContext } from 'react';
-
-import { ProductsContext } from '../../../context/products.context';
-import ProductCard from '../../product-card/product-card.component';
+import {Routes, Route } from 'react-router-dom';
 import './shop.styles.scss'
 
-const Shop = () => {
-const { products } = useContext(ProductsContext);
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
-return (
-    <div className='products-container'>
-        {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
-)
+const Shop = () => {
+
+    return (
+       <Routes>
+        <Route index element={<CategoriesPreview />} />
+        <Route path=":category" element={<Category />} />
+       </Routes>
+    )
 }
 
 export default Shop;
