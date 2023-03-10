694341f4793f30cac11fab4a0d8e79e6c45cdb04
Stefan Scrob <stefan.scrob2@klarna.com>
2023-03-10 16:12:29 +0200

    adding redirect for netlify

--- src/components/routes/category/category.component.jsx
+++ src/components/routes/category/category.component.jsx
@@ -1,12 +1,11 @@
 import './category.styles.scss'
 import { useParams } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../../context/categories.context';
 import ProductCard from '../../product-card/product-card.component';
 
 const Category = () => {
   const { category } = useParams();
-  console.log(category)
   const { categoriesMap } = useContext(CategoriesContext);
   const [ products, setProducts ] = useState(categoriesMap[category]);
 
@@ -15,11 +14,14 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
+    <Fragment>
+      <h2 className='category-title'>{category.toUpperCase()}</h2>
     <div className='category-container'>
       {products &&
         products.map((product) => (<ProductCard key={product.id} product={product} />))
       }
     </div>
+    </Fragment>
   )
 }
 
